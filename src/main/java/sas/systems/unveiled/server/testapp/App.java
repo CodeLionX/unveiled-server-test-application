e3bc6f6db07462aad7858cc0adb0a66d775964af
CodeLionX <sebastian.schmidl@t-online.de>
2016-05-20 23:38:51 +0200

    initial commit

---
+++ src/main/java/sas/systems/unveiled/server/testapp/App.java
@@ -0,0 +1,11 @@
+package sas.systems.unveiled.server.testapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args ) {
+        System.out.println("Hello World!" );
+    }
+}
